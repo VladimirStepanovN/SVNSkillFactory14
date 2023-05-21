cea89e45b140ea730e978e51cd7670dd9bbc6c3b
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-22 01:05:59 +0500

    Задание 4.1.14.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,28 @@
 ﻿namespace SVNSkillFactory14
 {
-    ///<summary>
-    ///4.1.12. Измените код выше так, чтобы отобразилось значение «условие истинно».
-    ///</summary>
-    internal class Program
+	///<summary>
+	///4.1.14. Какой отобразится результат?
+	///</summary>
+	internal class Program
     {
         static void Main(string[] args)
         {
-            var a = 6;
-            var b = 6;
+			var a = 6;
+			var b = 7;
 
-            if (a == b)
-            {
-                Console.WriteLine("Условие истинно");
-            }
-            else
-            {
-                Console.WriteLine("Условие ложно");
-            }
-        }
+			if (a == b && b > 1)
+			{
+				Console.WriteLine("Условие истинно");
+			}
+
+			else if (b > 10 || b == 7)
+			{
+				Console.WriteLine("Значение b = {0} больше 10 или равно 7", b);
+			}
+			else
+			{
+				Console.WriteLine("Значение b = {0}", b);
+			}
+		}
     }
 }
