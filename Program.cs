cc8aa5d3c5ae00ca6ad5f6ecd9053c89131016d7
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-22 01:00:37 +0500

    Задание 4.1.11.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,24 @@
 ﻿namespace SVNSkillFactory14
 {
     ///<summary>
-    ///4.1.10. Измените код в приложении выше таким образом,
-    ///чтобы в консоли отобразился результат переменной result.
-    ///В окне снизу введите результат выполнения программы.
+    ///4.1.11. Запустите приложение выше и посмотрите на результат в консольном сообщении. 
+    ///Данный результат введите в окно снизу.
     ///</summary>
     internal class Program
     {
         static void Main(string[] args)
         {
-            var inv = true;
-            var result = !inv;
-            Console.WriteLine(result);
+            var a = 6;
+            var b = 7;
+
+            if (a == b)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
         }
     }
 }
