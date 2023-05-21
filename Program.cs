284168eabfdf8e83148f9b6741fc624c7f1aa149
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-22 00:50:42 +0500

    Задание 4.1.10.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace SVNSkillFactory14
 {
+    ///<summary>
+    ///4.1.10. Измените код в приложении выше таким образом,
+    ///чтобы в консоли отобразился результат переменной result.
+    ///В окне снизу введите результат выполнения программы.
+    ///</summary>
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var inv = true;
+            var result = !inv;
+            Console.WriteLine(result);
         }
     }
 }
