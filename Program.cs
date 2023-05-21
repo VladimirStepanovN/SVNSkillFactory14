cb5ca5ea6c6c006e7e6a0ff63305899668eff576
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-22 01:23:13 +0500

    Задание 4.1.18.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿namespace SVNSkillFactory14
 {
 	///<summary>
-	///4.1.17.Внесите программу в репозиторий, 
-	///сделайте коммит об изменениях с указанием номера юнита.
+	///4.1.18. Добавьте в программу обработку ввода цвета cyan,
+	///а в блоке default поменяйте на желтый цвет цвет фона,
+	///а цвет текста — на красный.
 	///</summary>
 	internal class Program
     {
@@ -12,27 +13,35 @@
 
 			var color = Console.ReadLine();
 
-			if (color == "red")
+			switch (color)
 			{
-				Console.BackgroundColor = ConsoleColor.Red;
-				Console.ForegroundColor = ConsoleColor.Black;
+				case "red":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is red!");
-			}
+					Console.WriteLine("Your color is red!");
+					break;
 
-			else if (color == "green")
-			{
-				Console.BackgroundColor = ConsoleColor.Green;
-				Console.ForegroundColor = ConsoleColor.Black;
+				case "green":
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is green!");
-			}
-			else
-			{
-				Console.BackgroundColor = ConsoleColor.Cyan;
-				Console.ForegroundColor = ConsoleColor.Black;
+					Console.WriteLine("Your color is green!");
+					break;
 
-				Console.WriteLine("Your color is cyan!");
+				case "cyan":
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is cyan!");
+					break;
+
+				default:
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine("Your color is yellow!");
+					break;
 			}
 		}
     }
