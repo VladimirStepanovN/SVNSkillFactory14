662c4229a230374779c3913e6d111194b472cd2a
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-23 00:55:34 +0500

    Задание 4.3.15.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,20 @@
 ﻿namespace SVNSkillFactory14
 {
 	///<summary>
-	///4.3.14. Сделайте перебор данного массива массивов по аналогии с массивом строк.
+	///4.3.15. Задайте одномерный массив и напишите алгоритм,
+	///который находит в нем количество положительных чисел.
 	///</summary>
 	internal class Program
     {
 		static void Main(string[] args)
 		{
-			int[][] array = new int[3][];
-			array[0] = new int[2] { 1, 2 };
-			array[1] = new int[3] { 1, 2, 3 };
-			array[2] = new int[5] { 1, 2, 3, 4, 5 };
-			foreach(var arr in array)
+			var arr = new int[] { -5, 6, 9, -1, 2, -3, -4 };
+			int nElems = 0;
+			foreach(int elem in arr)
             {
-				foreach(int i in arr)
-                {
-                    Console.Write($@"{i} ");
-                }
+				nElems += elem > 0 ? 1 : 0;
             }
+            Console.WriteLine(nElems);
 		}
     }
 }
