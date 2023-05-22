99079604f88753beea0075c3fa05107f9183ef51
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-23 00:50:14 +0500

    Задание 4.3.14.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
 ﻿namespace SVNSkillFactory14
 {
 	///<summary>
-	///4.3.13. Для заданного массива:
-	///var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-	///Необходимо найти сумму всех его элементов.
+	///4.3.14. Сделайте перебор данного массива массивов по аналогии с массивом строк.
 	///</summary>
 	internal class Program
     {
 		static void Main(string[] args)
 		{
-			var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-			int sum = 0;
-			foreach(var i in arr)
+			int[][] array = new int[3][];
+			array[0] = new int[2] { 1, 2 };
+			array[1] = new int[3] { 1, 2, 3 };
+			array[2] = new int[5] { 1, 2, 3, 4, 5 };
+			foreach(var arr in array)
             {
-				sum += i;
+				foreach(int i in arr)
+                {
+                    Console.Write($@"{i} ");
+                }
             }
-            Console.WriteLine(sum);
 		}
     }
 }
