590eccf8887d89497ec784aef3ba2dd3813e2be1
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-23 01:35:41 +0500

    Задание 4.3.17. Сортировка строк двумерного массива методом вставки.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,53 @@
 ﻿namespace SVNSkillFactory14
 {
 	///<summary>
-	///4.3.16. Давайте модифицируем нашу предыдущую задачу для работы с двумерным массивом.
-	///Необходимо найти количество положительных элементов массива.
+	///4.3.17. Одной из нетривиальных задач является задача сортировки двумерного массива.
+	///Как минимум, не слишком понятно, как же сортировать, есть ведь «строки» и «столбцы».
+	///Как требуется проводить сортировку?
+	///В данном случае давайте реализуем просто сортировку элементов массива внутри одной строки.
 	///</summary>
 	internal class Program
     {
 		static void Main(string[] args)
 		{
 			int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-			int nElems = 0;
-			foreach(int i in arr)
-            {
-				nElems += i > 0 ? 1 : 0;
-            }
-            Console.WriteLine(nElems);
+			//------------------------------вывод до-----------------------------------------------
+			for(int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+			{
+				for(int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+				{
+                    Console.Write($@"{arr[i, j]} ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+			Console.WriteLine();
+			//------------------------------сортировка вставкой------------------------------------
+			int inner, outer, row;
+			for (row = 0; row < arr.GetUpperBound(0) + 1; row++)
+			{
+				int temp;
+				for (outer = 1; outer < arr.GetUpperBound(1) + 1; outer++)
+				{
+					inner = outer;
+					temp = arr[row, outer];
+					while (inner > 0 && arr[row, inner - 1] >= temp)
+					{
+						arr[row, inner] = arr[row, inner - 1];
+						--inner;
+					}
+					arr[row, inner] = temp;
+				}					
+			}
+			//------------------------------вывод после--------------------------------------------
+			for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+			{
+				for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+				{
+					Console.Write($@"{arr[i, j]} ");
+				}
+                Console.WriteLine();
+			}
 		}
     }
 }
