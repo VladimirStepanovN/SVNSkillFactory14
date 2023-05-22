bd8a84cede178904be6af281c671b82789349f4d
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-23 00:15:58 +0500

    Задание 4.3.13.

--- Program.cs
+++ Program.cs
@@ -1,31 +1,21 @@
 ﻿namespace SVNSkillFactory14
 {
 	///<summary>
-	///4.3.12. Эта задача является одной из базовых задач при работе с массивами — 
-	///сортировка одномерного массива.
-	///Вам необходимо отсортировать его в порядке возрастания цифр и вывести на экран.
+	///4.3.13. Для заданного массива:
+	///var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+	///Необходимо найти сумму всех его элементов.
 	///</summary>
 	internal class Program
     {
 		static void Main(string[] args)
 		{
 			var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-			int inner, outer;
-			for(outer = 1; outer < arr.Length; outer++)
+			int sum = 0;
+			foreach(var i in arr)
             {
-				int temp = arr[outer];
-				inner = outer;
-				while(inner > 0 && arr[inner - 1] >= temp)
-                {
-					arr[inner] = arr[inner - 1];
-					--inner;
-                }
-                arr[inner] = temp;
+				sum += i;
             }
-            foreach(int i in arr)
-            {
-                Console.Write($@"{i} ");
-            }
+            Console.WriteLine(sum);
 		}
     }
 }
