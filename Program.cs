0a1ea6941e0abe7a54502b67064924f395cb457a
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-23 00:59:53 +0500

    Задание 4.3.16.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 ﻿namespace SVNSkillFactory14
 {
 	///<summary>
-	///4.3.15. Задайте одномерный массив и напишите алгоритм,
-	///который находит в нем количество положительных чисел.
+	///4.3.16. Давайте модифицируем нашу предыдущую задачу для работы с двумерным массивом.
+	///Необходимо найти количество положительных элементов массива.
 	///</summary>
 	internal class Program
     {
 		static void Main(string[] args)
 		{
-			var arr = new int[] { -5, 6, 9, -1, 2, -3, -4 };
+			int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
 			int nElems = 0;
-			foreach(int elem in arr)
+			foreach(int i in arr)
             {
-				nElems += elem > 0 ? 1 : 0;
+				nElems += i > 0 ? 1 : 0;
             }
             Console.WriteLine(nElems);
 		}
