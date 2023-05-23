db0588ea7d18ff2d52413c85f32184974d5f7df9
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-23 23:19:50 +0500

    Задание 4.5.2.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿namespace SVNSkillFactory14
 {
 	///<summary>
-	///4.5.1. Задайте кортеж User, который описывает необходимую информацию.
+	///4.5.2. Заполните кортеж User значениями с клавиатуры для полей: имя пользователя, фамилия, логин.
 	///</summary>
 	internal class Program
     {
 		static void Main(string[] args)
 		{
 			(string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+			Console.WriteLine("Введите имя");
+			User.Name = Console.ReadLine();
+			Console.WriteLine("Введите фамилию");
+            User.LastName = Console.ReadLine();
+			Console.WriteLine("Введите логин");
+            User.Login = Console.ReadLine();
 		}
     }
 }
