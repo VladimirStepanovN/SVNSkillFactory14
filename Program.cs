d5a70aaec95364750ef799e1b46ed4ccfcda4a6d
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-23 23:04:23 +0500

    Задание 4.5.1.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,13 @@
 ﻿namespace SVNSkillFactory14
 {
 	///<summary>
-	///4.4.5. Дан следующий кортеж:
-	///(string Name, string Type, double Age, int NameCount) Pet;
-	///Name — имя питомца;
-	///Type — тип питомца: собака, рыбка, хомячок;
-	///Age — возраст питомца;
-	///NameCount — длина имени питомца.
-	///Заполните данный кортеж значениями аналогично примерам, разобранным в модуле выше.
+	///4.5.1. Задайте кортеж User, который описывает необходимую информацию.
 	///</summary>
 	internal class Program
     {
 		static void Main(string[] args)
 		{
-			(string Name, string Type, double Age, int NameCount) Pet;
-            Console.Write("Введите имя вашего питомца: ");
-			Pet.Name = Console.ReadLine();
-			Console.Write("Введите возраст вашего питомца: ");
-			Pet.Age = Double.Parse(Console.ReadLine());
-			Console.Write("Введите тип вашего питомца: ");
-			Pet.Type = Console.ReadLine();
-			Pet.NameCount = Pet.Name.Length;
-			Console.WriteLine($@"Имя вашего питомца: {Pet.Name}");
-			Console.WriteLine($@"Возраст вашего питомца: {Pet.Age}");
-            Console.WriteLine($@"Тип вашего питомца: {Pet.Type}");
-            Console.WriteLine($@"Длина имени вашего питомца: {Pet.NameCount}");
+			(string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
 		}
     }
 }
