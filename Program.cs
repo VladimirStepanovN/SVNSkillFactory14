4a361e6428cc5b85c6f14b79039f270b905bcde7
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-23 23:37:30 +0500

    Задание 4.5.6.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,33 @@
 ﻿namespace SVNSkillFactory14
 {
 	///<summary>
-	///4.5.5. Напишите код для ввода возраста пользователя и трех его любимых цветов.
+	///4.5.6. Сделайте так, чтобы опрос повторялся для трех пользователей.
 	///</summary>
 	internal class Program
     {
 		static void Main(string[] args)
 		{
-			(string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
-			Console.WriteLine("Введите имя");
-			User.Name = Console.ReadLine();
-			Console.WriteLine("Введите фамилию");
-            User.LastName = Console.ReadLine();
-			Console.WriteLine("Введите логин");
-            User.Login = Console.ReadLine();
-			User.LoginLength = User.Login.Length;
-			Console.WriteLine("Есть ли у вас животные? Да или Нет");
-			User.HasPet = "Да".Equals(Console.ReadLine());
-			Console.WriteLine("Введите возраст пользователя");
-			User.Age = double.Parse(Console.ReadLine());
-			User.favcolors = new string[3];
-			Console.WriteLine("Введите три любимых цвета пользователя");
-			for(int i = 0; i < User.favcolors.Length; i++)
-            {
-                User.favcolors[i] = Console.ReadLine();
-            }
+			for (int t = 0; t < 3; t++) 
+			{
+				(string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+				Console.WriteLine("Введите имя");
+				User.Name = Console.ReadLine();
+				Console.WriteLine("Введите фамилию");
+				User.LastName = Console.ReadLine();
+				Console.WriteLine("Введите логин");
+				User.Login = Console.ReadLine();
+				User.LoginLength = User.Login.Length;
+				Console.WriteLine("Есть ли у вас животные? Да или Нет");
+				User.HasPet = "Да".Equals(Console.ReadLine());
+				Console.WriteLine("Введите возраст пользователя");
+				User.Age = double.Parse(Console.ReadLine());
+				User.favcolors = new string[3];
+				Console.WriteLine("Введите три любимых цвета пользователя");
+				for (int i = 0; i < User.favcolors.Length; i++)
+				{
+					User.favcolors[i] = Console.ReadLine();
+				}
+			}
 		}
     }
 }
